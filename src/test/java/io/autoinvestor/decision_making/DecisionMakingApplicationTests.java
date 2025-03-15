be0fe329fed58d769ce8f6df97787a90291462e6
Pol Piñol Castuera <pol.pinolcastuera@gmail.com>
2025-03-15 20:15:21 +0100

    feat: initial structure for the project (#1)

---
+++ src/test/java/io/autoinvestor/decision_making/DecisionMakingApplicationTests.java
@@ -0,0 +1,13 @@
+package io.autoinvestor.decision_making;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecisionMakingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
