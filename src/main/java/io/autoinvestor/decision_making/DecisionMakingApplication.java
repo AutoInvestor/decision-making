7ca8713adc25fc34310cd5837b9a3112d599ef1e
Pol Pinol Castuera <pol.pinol@mongodb.com>
2025-03-15 20:08:52 +0100

    Initial structure

---
+++ src/main/java/io/autoinvestor/decision_making/DecisionMakingApplication.java
@@ -0,0 +1,13 @@
+package io.autoinvestor.decision_making;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecisionMakingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecisionMakingApplication.class, args);
+	}
+
+}
