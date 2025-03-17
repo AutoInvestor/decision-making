d51f390a3786a6f61016844980e9ae57188a4a57
Pol Pinol Castuera <pol.pinol@mongodb.com>
2025-03-17 19:41:48 +0100

    Initial structure and pipelines

--- src/test/java/io/autoinvestor/DecisionMakingApplicationTests.java
+++ src/test/java/io/autoinvestor/DecisionMakingApplicationTests.java
@@ -1,4 +1,4 @@
-package io.autoinvestor.decision_making;
+package io.autoinvestor;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DecisionMakingApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
