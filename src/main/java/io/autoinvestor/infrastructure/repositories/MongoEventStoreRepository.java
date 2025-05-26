2ab0b8001bf3e5eff0464c929d8db6906a3d88fa
Pol Piñol Castuera <pol.pinolcastuera@gmail.com>
2025-05-26 09:15:36 +0200

    fix: use aggregate id for event store (#20)

--- src/main/java/io/autoinvestor/infrastructure/repositories/MongoEventStoreRepository.java
+++ src/main/java/io/autoinvestor/infrastructure/repositories/MongoEventStoreRepository.java
@@ -39,7 +39,7 @@ public class MongoEventStoreRepository implements EventStoreRepository {
     @Override
     public Decision get(DecisionId decisionId) {
         Query q = Query.query(
-                        Criteria.where("decisionId")
+                        Criteria.where("aggregateId")
                                 .is(decisionId.toString())
                 )
                 .with(Sort.by("version"));
