c9c262479ae1fe33e225c2b64f2e539df74a8705
Pol Pinol Castuera <pol.pinol@mongodb.com>
2025-05-26 22:23:29 +0200

    Date not str

--- src/main/java/io/autoinvestor/domain/events/DecisionTakenEventPayload.java
+++ src/main/java/io/autoinvestor/domain/events/DecisionTakenEventPayload.java
@@ -1,16 +1,10 @@
 package io.autoinvestor.domain.events;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import java.util.Date;
 import java.util.Map;
 
 public record DecisionTakenEventPayload(
         String assetId,
-        @JsonFormat(
-                shape    = JsonFormat.Shape.STRING,
-                pattern  = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX",
-                timezone = "UTC"
-        )
         Date date,
         String decision,
         int riskLevel
